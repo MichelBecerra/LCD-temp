58279ab0adbff3bd2f3f5b956dfd1b7ee89a5870
Michel <michelandreabc@gmail.com>
2017-10-14 09:28:33 -0500

    Cambios minimos

--- LCD-temp.ino
+++ LCD-temp.ino
@@ -1,6 +1,5 @@
-#include <LiquidCrystal.h>
-
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+#include "Sensor.h"
+#define SEN_COUNT 4
 
 //States
 #define VIEW_S1   1
@@ -8,54 +7,48 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 #define EDIT_T    3
 #define SAVE_T    4
 
-// Declaracion de transiciones de estado
-void moveLeft();   //Mover a pantalla anterior
-void moveRight();  //Mover a pantalla siguiente
-
-// Clases
-
-class Sensor{
-  public:
-    Sensor(int temp1, int hum1);   //Contructor
-    int get_temp();
-    int get_humi();
-    int view_sensor(); 
-  private:
-    int temperature;
-    int humidity;
-};
-
-Sensor::Sensor(int temp1, int hum1)
-{
-  this->temperature = temp1;
-  this->humidity = hum1;
-}
-
-Sensor:: get_temp()
-{
-  return temperature;
-}
-
-
-Sensor:: get_humi()
-{
-  return humidity;
-}
-Sensor:: view_sensor()
-{
-  lcd.setCursor(0,0);
-  lcd.print("T: ");
-  lcd.print(temperature);
-  lcd.setCursor(0,1);
-  lcd.print("H: ");
-  lcd.print(humidity);
-}
-
 void setup() {
+  Serial.begin(9600);
   lcd.begin(16,2);
-  Sensor sensor1 = Sensor(50, 87);
-  sensor1.view_sensor();
-  
+  Sensor *arr = setValues();
+  views(arr);
+}
+
+Sensor* setValues()
+{
+    int temp1 = 0;
+    int hum1 = 0;
+    Sensor *arr_sensor;
+    for(int i = 0; i < SEN_COUNT; i++)
+    {
+      Serial.print("SERIAL ");
+      Serial.print(i+1);
+      Serial.println("\nIntroduce temperatura: ");
+      Serial.flush();
+      while(!Serial.available())
+      {
+        ;
+      }
+      temp1 = Serial.read();
+      Serial.println("Introduce humedad: ");
+      Serial.flush();
+      while(!Serial.available())
+      {
+        ;
+      }
+      hum1 = Serial.read();
+      arr_sensor[i] = Sensor(temp1, hum1);
+    } 
+    return arr_sensor;
+}
+
+void views(Sensor *arr)
+{
+  for(int i = 0; i < 4; i++)
+  {
+    arr[i].view_sensor();
+    delay(5000);
+  }
 }
 
 void loop() {
---
+++ Sensor.h
@@ -0,0 +1,43 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+
+class Sensor{
+  public:
+    Sensor();
+    Sensor(int temp1, int hum1);
+    int get_temp();
+    int get_humi();
+    int view_sensor(); 
+  private:
+    int temperature;
+    int humidity;
+    Sensor *next;
+};
+
+Sensor::Sensor(int temp1, int hum1)
+{
+  this->temperature = temp1;
+  this->humidity = hum1;
+}
+
+Sensor:: get_temp()
+{
+  return temperature;
+}
+
+Sensor:: get_humi()
+{
+  return humidity;
+}
+Sensor:: view_sensor()
+{
+  lcd.setCursor(1,0);
+  lcd.print("T: ");
+  lcd.print(temperature, DEC);
+  lcd.print(" H: ");
+  lcd.print(humidity, DEC);
+}
+#endif
